067d43a691cf1562508644c640b4bceda7ef55b8
Jon Lane <admiralorbiter@gmail.com>
2022-11-13 12:16:11 -0600

    added more classes

--- board.py
+++ board.py
@@ -9,19 +9,29 @@ class Board:
         for i in range(1, 12):
             self.board.append([i+1, 0, 0])
 
+    ### Roll Dice ###
+    # Helper function that takes number of dice and sides to roll and returns the results in a list
+    # EX: roll_dice(4,6) will roll 4 6-sided dice and return the results in a list
     def roll_dice(self,num_dice, num_sides):
         dice = []
         for i in range(num_dice):
             dice.append(random.randint(1, num_sides))
         return dice
 
-    def calc_single_odds(self, dice):
+    ### Calculate Dice Combinations ###
+    # Given the four dice, this function calculates all possible combinations of dice for a given move
+    # dice - list of four dice
+    def dice_combinations(self, dice):
         results=[]
         results.append((dice[0]+dice[1], dice[2]+dice[3]))
         results.append((dice[0]+dice[2], dice[1]+dice[3]))
         results.append((dice[0]+dice[3], dice[1]+dice[2]))
         return results
 
+    ### Print Board ###
+    # Prints the current board state in an easy to read format
+    # O - player 1 token
+    # X - player 2 token
     def print_board(self):
         print("Board:")
         for i in range(0, 10):
@@ -33,34 +43,37 @@ class Board:
                     print("X", end="")
             print("")
 
-    def evaluate_move(self, p, n):
+    ### Evaluates if the move is valid ###
+    # n - column number
+    # if the column is full, returns false, else returns true
+    def evaluate_move(self, n):
         if n==7:
-            if self.board[n-2][p]>=8:
+            if self.board[n-2][1]>=8 or self.board[n-1][2]>=8:
                 return False
             else:
                 return True
         elif n==8 or n==6:
-            if self.board[n-2][p]>=7:
+            if self.board[n-2][1]>=7 or self.board[n-1][2]>=7:
                 return False
             else:
                 return True
         elif n==9 or n==5:
-            if self.board[n-2][p]>=6:
+            if self.board[n-2][1]>=6 or self.board[n-1][2]>=6:
                 return False
             else:
                 return True
         elif n==10 or n==4:
-            if self.board[n-2][p]>=5:
+            if self.board[n-2][1]>=5 or self.board[n-1][2]>=5:
                 return False
             else:
                 return True
         elif n==11 or n==3:
-            if self.board[n-2][p]>=4:
+            if self.board[n-2][1]>=4 or self.board[n-1][2]>=4:
                 return False
             else:
                 return True
         elif n==12 or n==2:
-            if self.board[n-2][p]>=3:
+            if self.board[n-2][1]>=3 or self.board[n-1][2]>=3:
                 return False
             else:
                 return True
--- test_board.py
+++ test_board.py
@@ -14,6 +14,41 @@ def initialization():
     assert b.board[8]==[10, 0, 0]
     assert b.board[9]==[11, 0, 0]
     assert b.board[10]==[12, 0, 0]
+
+def test_roll_dice():
+    board = Board()
+    roll=board.roll_dice(4, 6)
+    assert len(roll)==4
+    assert roll[0]>=1
+
+
+def test_dice_combinations():
+    board = Board()
+    dice = [1, 2, 3, 4]
+    combos = board.dice_combinations(dice)
+    assert combos[0] == (3, 7)
+    assert combos[1] == (4, 6)
+    assert combos[2] == (5, 5)
+    #TODO: Test if dice list is larger or smaller
+
+def test_evaluate_move():
+    board = Board()
+    assert board.evaluate_move(2) == True
+    assert board.evaluate_move(3) == True
+    assert board.evaluate_move(4) == True
+    assert board.evaluate_move(5) == True
+    assert board.evaluate_move(6) == True
+    assert board.evaluate_move(7) == True
+    assert board.evaluate_move(8) == True
+    assert board.evaluate_move(9) == True
+    assert board.evaluate_move(10) == True
+    assert board.evaluate_move(11) == True
+    assert board.evaluate_move(12) == True
+    #TODO: Change board state to test if move is invalid
+    # Will need to test if move is invalid for each column and player combination
 if __name__ == "__main__":
     initialization()
+    test_roll_dice()
+    test_dice_combinations()
+    test_evaluate_move()
     print("All tests passed!")
